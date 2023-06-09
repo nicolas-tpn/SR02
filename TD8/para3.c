1a33585523e8458a5b2be739253c8b61f4a8d687
nico-tpn <nicolas.taupin312@gmail.com>
2023-06-09 12:11:26 +0200

    para3 fonctionnelgit status (plus ou moins)

--- TD8/para3.c
+++ TD8/para3.c
@@ -6,57 +6,44 @@
 
 #define MAX 10000
 
-struct data_thread {
-	pthread_t thread;
-	int numero;
-	int lower_bound;
-	int upper_bound;
-	int index;  
-};
-typedef struct data_thread data_thread;
-
 void creat_thread(int k, int nb_max);
 void *thread_execution(void *num);
 
 int k, n;
-//data_thread thread_array[MAX];
 pthread_t thread_array[MAX];
 
 int array[MAX];
-pthread_mutex_t mutex;
-pthread_cond_t condition;
-int activeThreads; // Nombre de threads actifs
 
 void* thread_execution(void *num) {
 	int lower_bound;
 	int upper_bound;
 	int* value = (int*)num;
 	
-	for (int i = 2; i<=sqrt(n); i++) {
+	for (int j = 2; j<=floor(sqrt(n)); j++) {
 		//Calcul slices 
-		int elements = ceil((n-(i*i))/i);
-		//printf("\n elements %d\n",elements);
-		lower_bound = i*i + (*value)*elements/k;
-		upper_bound = i*i + (*value+1)*elements/k;
-		printf("Thread numero %d : [%d; %d] \n",*value, lower_bound, upper_bound);
+		int nb_elem = ceil((n-(j*j))/j);
+		int chunk = ceil(n-(j*j)/k);
+		
+		lower_bound = j*j + (*value)*chunk;
+		upper_bound = j*j + (*value+1)*chunk;
+		// Ligne pour debug/verification des valeurs des bornes
+		//printf("Thread numero %d : [%d; %d] \n",*value, lower_bound, upper_bound);
+		
+		for (int h = lower_bound; h<upper_bound; h+=j) {
+			array[h] = 0;	
+		}
 	}
-	
-	return NULL;
-	
+
+	pthread_exit(NULL);
+
 }
 
 void creat_thread(int k, int nb_max) {
-
-	pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&condition, NULL);
-
-    activeThreads = k;
 	
 	if (k > nb_max) {
 		//on créé nb_max threads
 		for (int i = 0; i<nb_max; i++) {
 			int numero = i;
-			printf("\n creation de thread opt 1\n");
 			
 			if ((pthread_create(&(thread_array[i]), NULL, thread_execution, &numero))) {
 				fprintf(stderr, "pthread_create error \n");
@@ -67,10 +54,10 @@ void creat_thread(int k, int nb_max) {
 	} else {
 		//on créé k threads
 		for (int i = 0; i<k; i++) {
-			int numero = i;
-			//printf("\n Creation de thread opt 2\n");
+			int* numero = malloc(sizeof(int));
+			*numero = i;
 			printf("%d\n", i);
-			if ((pthread_create(&(thread_array[i]), NULL, thread_execution, &numero))) {
+			if ((pthread_create(&(thread_array[i]), NULL, thread_execution, numero))) {
 				fprintf(stderr, "pthread_create error \n");
 				exit(EXIT_FAILURE);
 			}
@@ -99,6 +86,16 @@ int main (int argc, char* argv[]) {
 	double nb_elements_max = ceil((n-4)/2);
 	creat_thread(k, nb_elements_max);
 
+	for (int w = 0; w<k; w++) {
+		pthread_join(thread_array[w], NULL);
+	}
+
+	printf("Les nombres premiers nombres presents avant %d sont : \n", n);
+	for (int l = 2; l<n; l++) {
+		if (array[l] == 1) {
+			printf("%d\n", l);
+		}
+	}
 
 	return 0;
 

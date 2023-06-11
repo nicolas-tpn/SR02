8e3652ef0463435467efa304524af986792ca00a
nico-tpn <nicolas.taupin312@gmail.com>
2023-06-11 20:51:28 +0200

    bug fix

--- TD8/tache_4.sh
+++ TD8/tache_4.sh
@@ -8,11 +8,11 @@ do
 for (( i=1; i<=$iterations; i++ ))
 do
   ./tache_3 $NUM_THREADS $N
-done > time_exe.txt
+done > output_files/time_exe.txt
 
-sum=$(cat time_exe.txt | awk '{ sum += $1 } END { print sum }')
-count=$(cat time_exe.txt | wc -l)
+sum=$(cat output_files/time_exe.txt | awk '{ sum += $1 } END { print sum }')
+count=$(cat output_files/time_exe.txt | wc -l)
 mean=$(echo "scale=7; $sum / $count" | bc)
 
-echo "Mean exec, N = $N, $NUM_THREADS thread(s) --> $mean sec" >> output.txt
+echo "Mean exec, N = $N, $NUM_THREADS thread(s) --> $mean sec" >> output_files/output.txt
 done

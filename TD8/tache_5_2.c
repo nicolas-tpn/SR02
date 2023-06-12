1eb35fcf0d97cce26187dda5eaf136dc2ffcf9e7
ryduaaa <131461463+ryduaaa@users.noreply.github.com>
2023-06-12 23:39:57 +0200

    tache 5

--- TD8/tache_5_2.c
+++ TD8/tache_5_2.c
@@ -10,10 +10,11 @@ void *thread_execution(void *num);
 
 clock_t start, end;
 double cpu_time_used;
-int compteur = 0;
+int compteur = 1;
 int k, n;
 pthread_t *thread_array;
 int *array;
+
 
 void *thread_execution(void *num) {
 	int lower_bound;
@@ -24,17 +25,20 @@ void *thread_execution(void *num) {
 	for (int j = 2; j < sqrt(n); j++) {
 
 		//Calcul slices 
-		int nb_elem = ceil(((n+1)/2-(j*j))/j);
+		int nb_elem = ceil((n-(j*j))/j);
 		
 		lower_bound = ceil(j * j + (nb_elem * value / k) * j);
 		upper_bound = ceil(j * j + (nb_elem * (value + 1) / k) * j);
-		//printf("%d , %d\n", lower_bound/2, upper_bound/2);
-		int tmp = value-1;
-		for (int h = lower_bound; h <= upper_bound; h += 2*j) {
+		
+
+		for (int h = lower_bound; h <= upper_bound; h += j ) {
+			if (h%2 != 0){
+				int e;
+				e = (h/2) - 1;
+
+				array[e] = 0;
 			
-			array[h] = 0;
-			printf("\n h = %d \n",tmp);
-			tmp++;
+			}
 			
 		}
 	}
@@ -69,15 +73,19 @@ int main (int argc, char* argv[]) {
     k = atoi(argv[1]);
     n = atoi(argv[2]);
 
-    array = malloc(ceil((n+1)/2)*sizeof(int));
+    array = malloc(((n+1)/2)*sizeof(int));
     thread_array = malloc((k)*sizeof(pthread_t));
 	
 	start = clock();
 
 	// Initialisation du tableau booléen (1 pour vrai, 0 pour faux)
-	for (int i = 2; i<(n+1)/2; i++) {
+	array[0] = 1; //3
+	array[1] = 1; //5
+	array[2] = 1; //7
+	for (int i = 3; i<(n+1)/2; i++) {
 		array[i] = 1;
 	}
+	
 	// Création des threads
 	double nb_elements_max = ceil((n-4)/2);
 	creat_thread(k, nb_elements_max);
@@ -86,18 +94,20 @@ int main (int argc, char* argv[]) {
 		pthread_join(thread_array[w], NULL);
 	}
 
-	//printf("Les nombres premiers nombres presents avant %d sont : \n", n);
-	for (int l = 2; l < (n+1)/2; l++) {
+	printf("Les nombres premiers avant %d sont : \n", n);
+	printf("2\n");
+	for (int l = 0; l < ((n+1)/2)-1; l++) {
 		if (array[l] == 1) {
 			// Vérification des valeurs des nombres premiers
-			printf(" 2l + 3 :%d\n", 2*l+3);
-			printf("l : %d\n", l);
+			printf("%d\n", 2*l+3);
 			compteur++;
 		}
 	}
+	
+	
 
 	// Verification du nombre de nombres premiers
-	//printf("\n Il y a %d nombres premiers", compteur);
+	printf("\nIl y a %d nombres premiers\n", compteur);
 
 	end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
